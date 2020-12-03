d1b05adff9709d146b25726af37ffe14b3f750f6
William R <wiribasczky@gmail.com>
2020-12-03 19:44:20 -0300

    Atualizado views de exemplo
    Problema/Proposito
    - Inclusão de dados nas views para fins de demonstração

--- templates/products/index.tpl.php
+++ templates/products/index.tpl.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Users</title>
+    <title>Products</title>
 </head>
 <body>
-    <h1>Olá <?= $nome ?></h1>
+<h1>Tabela usada: <?= $table ?></h1>
 </body>
 </html>
